a802cd12b06fbc0a7c1c586141a8006fb1c1d6ee
DilyanaStoyanova <dilyanazhstoyanova@gmail.com>
2023-11-22 13:44:07 +0200

    Practice fundamental concepts

--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -27,6 +27,7 @@ const isIsland = false;
 const language = 'Bulgarian';
 */
 /*
+Basic operators
 let population = 8000000;
 console.log(population / 2);
 population++;
@@ -38,3 +39,42 @@ const continent = 'Europe';
 const language = 'Bulgarian';
 const description = country + ' is in ' + continent + ', and its ' + population / 1000000 + ' million people speak ' + language;
 console.log(description);*/
+/* 
+String and template literals
+let population = 8000000;
+const country = 'Bulgaria';
+const continent = 'Europe';
+const language = 'Bulgarian';
+const description = `${country} is in ${continent}, and its ${population / 1000000} million people speak ${language}`;
+console.log(description);
+*/
+/* If else statements
+let population = 8000000;
+const country = 'Bulgaria';
+if (population > 33000000) {
+    console.log(`${country}'s population is above average`);
+} else {
+    const diff = 33000000 - population;
+    console.log(`${country}'s population is ${diff / 1000000} million below average`);
+}
+*/
+/*
+Type conversion and coercion
+console.log('9' - '5');
+console.log('19' - '13' + '17');
+console.log('19' - '13' + 17);
+console.log('123' < 57);
+console.log(5 + 6 + '4' + 9 - 4 - 2);
+*/
+/*
+Equality operators
+const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+
+if (numNeighbours === 1) {
+    console.log('Only 1 border!');
+} else if (numNeighbours > 1) {
+    console.log('More than 1 border')
+} else {
+    console.log('No borders');
+}
+*/
--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -177,7 +177,8 @@ if (birthYear <= 2000) {
 
 console.log(century);
 */
-/* Challenge
+//Challenge
+/*
 const massMark = 78;
 const heightMark = 1.69;
 const massJohn = 92;
@@ -204,6 +205,188 @@ if (BMIMark > BMIJohn) {
 
 console.log(output);
 */
+//////////////////////////////////
+// Type conversion
+/*
+const inputYear = '1986';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Dilyana'));
+console.log(typeof NaN);
+
+console.log(String(38), 38);
+// Type coercion
+console.log('I\'m ' + 38 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+
+let n = '1' + 1;
+n--;
+console.log(n);
+
+console.log(2 + 3 + 4 + '5');
+console.log('10' - '4' - '3' - 2 + '5');
+*/
+//////////////////////////////////
+//Truthy and false values
+/*
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+
+const money = 10;
+
+if (money) {
+    console.log('Please don\'t spend it all');
+} else {
+    console.log('You should get a job');
+}
+
+let height = 0;
+
+if (height) {
+    console.log('Height is defined');
+} else {
+    console.log('Height is undefined');
+}
+*/
+//////////////////////////////////
+//Equality operators
+/*
+const age = "18";
+
+if (age === 18) console.log('You just became an adult (strict)');
+if (age == 18) console.log('You just became an adult (loose)');
+
+const favourite = Number(prompt('What is your favourite number?'));
+console.log(favourite);
+
+if (favourite === 9) {
+    console.log('9 is an amazing number!');
+} else if (favourite === 7) {
+    console.log('7 is also a cool number');
+} else if (favourite === 23) {
+    console.log('23 is also a cool number');
+} else {
+    console.log('Number is not 7 or 9 or 23');
+}
+
+if (favourite !== 23) console.log('Why not 23');
+*/
+//////////////////////////////////
+//Logical operators
+/*
+const hasDriversLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+if (hasDriversLicense && hasGoodVision) {
+    console.log('Sarah is able to drive!');
+} else {
+    console.log('Someone else should drive...');
+}
+
+const isTired = false;
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log('Sarah is able to drive');
+} else {
+    console.log('Someone else should drive...');
+}
+*/
+//Challenge
+/*
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log('Dolphins win the trophy');
+} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+    console.log('Koalas win the trophy');
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100) {
+    console.log('Both win the trophy');
+} else {
+    console.log('No one wins the trophy');
+}
+*/
+//////////////////////////////////
+//Switch statement
+/*
+const day = 'Friday';
+
+switch (day) {
+    case 'Monday':
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'Tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'Wednesday':
+    case 'Thursday':
+        console.log('Write code examples');
+        break;
+    case 'Friday':
+        console.log('Record videos');
+        break;
+    case 'Saturday':
+    case 'Sunday':
+        console.log('Enjoy the weekend');
+        break;
+    default:
+        console.log('Not a valid day');
+        break;
+}
+
+if (day === 'Monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'Tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'Wednesday' || day === 'Thursday') {
+    console.log('Write code examples');
+} else if (day === 'Friday') {
+    console.log('Record videos');
+} else if (day === 'Saturday' || day === 'Sunday') {
+    console.log('Enjoy the weekend');
+} else {
+    console.log('Not a valid day');
+}
+*/
+//////////////////////////////////
+//Ternary operator
+/*
+const age = 8;
+
+age >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water');
+
+const drink = age >= 18 ? 'I like to drink wine' : 'I like to drink water';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
+}
+
+console.log(drink2);
+
+console.log(`i like to drink ${age >= 18 ? 'wine' : 'water'}`);
+*/
+//Challenge
+
+const bill = 275;
+
+const tip = bill * (bill >= 50 && bill <= 300 ? 0.15 : 0.20);
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
 
 
 
